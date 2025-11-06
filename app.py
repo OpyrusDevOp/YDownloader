2454c17603dd10b8ebd8458172227328432d45a8
OpyrusDevOp <opyrusdeveloper@gmail.com>
2025-11-06 04:26:56 +0100

    Add: Video get information

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask, request
+from pytubefix import YouTube
+from werkzeug.exceptions import BadRequest
 
 app = Flask(__name__)
 
@@ -6,3 +8,14 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+@app.route("/videoinfo")
+def get_video_info():
+    url = request.args.get("url")
+    if not url:
+        return BadRequest("No video url Provided")
+
+    vid = YouTube(url)
+
+    return f"Title : {vid.title} | Author : {vid.author}"
